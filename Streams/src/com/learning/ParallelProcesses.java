a20500731cd477a78c28674b828aa53e0143a028
siva <sivakumar179p@gmail.com>
2024-06-28 10:18:09 +0530

    update streams

---
+++ Streams/src/com/learning/ParallelProcesses.java
@@ -0,0 +1,21 @@
+package com.learning;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ParallelProcesses {
+
+	public static void main(String[] args) {
+
+	List<Integer> preOpe = IntStream.range(0, 1000000).boxed().collect(Collectors.toList());
+	long startTime = System.currentTimeMillis();
+	System.out.println(" StartTime :  " + startTime);
+		List<Integer> postOpe = preOpe.parallelStream().map(e->e+e).sorted().filter(e->e%2==0).collect(Collectors.toList());
+		long endTime = System.currentTimeMillis();
+		System.out.println(" endTime :  " + endTime);
+		System.out.println("Size of postOpe (Parallel): " + postOpe.size()); 
+		System.out.println("First 10 elements (Parallel): " + postOpe.subList(0, 10));
+	}
+
+}
---
+++ Streams/src/com/learning/SequencialProcesses.java
@@ -0,0 +1,23 @@
+package com.learning;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SequencialProcesses {
+
+	public static void main(String[] args) {
+
+		List<Integer> preOpe = IntStream.range(0, 1000000).boxed().collect(Collectors.toList());
+		long startTime = System.currentTimeMillis();
+		System.out.println(" StartTime :  " + startTime);
+		List<Integer> postOpe = preOpe.stream().map(e->e+e).sorted().filter(e->e%2==0).collect(Collectors.toList());
+		long endTime = System.currentTimeMillis();
+		System.out.println(" EndTime : " + endTime);
+		
+		System.out.println("Size of postOpe (sequential): " + postOpe.size()); 
+		System.out.println("First 10 elements (sequential): " + postOpe.subList(0, 10));
+	}
+		
+	}
+
